7b2b4ad162ca646eae30c71778268c34c44383fb
mronian <anchitn@gmail.com>
2015-04-16 17:29:06 +0530

    Changed field to NGram

--- facerec/jobs/search_indexes.py
+++ facerec/jobs/search_indexes.py
@@ -3,7 +3,7 @@ from haystack import indexes
 from .models import Camera
 
 class CamIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    text = indexes.NgramField(document=True, use_template=True)
     price=indexes.IntegerField(model_attr='price', faceted=True)
     avgrating=indexes.FloatField(model_attr='avg_rating', faceted=True)
     numrat=indexes.IntegerField(model_attr='number_of_ratings', faceted=True)
